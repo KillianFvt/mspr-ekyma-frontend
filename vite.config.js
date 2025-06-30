6345a3ac722cd8c751e31fa5272e601685f50f09
KillianFvt <killian.fauvette@gmail.com>
2025-06-30 17:41:36 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
